0db682854592ebf6545a23cb192196d62e30c68c
Florine Saidi <florine.saidi@efrei.net>
2024-11-28 11:28:40 +0100

    css mofifier

--- view/header.php
+++ view/header.php
@@ -6,25 +6,41 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
     <title>SmartBike</title>
-    <link rel="stylesheet" href="https://bootswatch.com/5/lumen/bootstrap.css">
+    <link rel="stylesheet" href="https://bootswatch.com/5/lux/bootstrap.css">
 
 </head>
 <body>
-    <nav>
-        <ul class="nav nav-pills">
-            <li>
-                <a href="?page=accueil">
+<nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
+  <div class="container-fluid">
+  <a href="?page=accueil">
                     <img src="images/logo.jpg" alt="logo entreprise SmartBike" height="50px">
                 </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="?page=accueil">Accueil</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="?page=produits">Vélos</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="?page=contact">Contacts</a>
-            </li>
-        </ul>
-    </nav>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarColor01">
+      <ul class="navbar-nav me-auto">
+        
+        <li class="nav-item">
+          <a class="nav-link" href="#">Accueil</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Vélos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contacts</a>
+        </li>
+        
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-sm-2" type="search" placeholder="Search">
+        <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+
+    
+            
+            
