eb09d1f571d97dd35d238bac41d4d35997603728
Florine Saidi <florine.saidi@efrei.net>
2024-11-28 10:47:06 +0100

    test

---
+++ view/contact.php
@@ -0,0 +1,19 @@
+
+<h1>Contact</h1>
+<from action="" method="post">
+
+Nom : <input type="text" name="nom"required> <br>
+Prenom : <input type="text" name="prenom"required> <br>
+Tel : <input type="text" name="tel" required> <br>
+Email : <input type="email" name="email" required > <br>
+message  : <input type="text" name ="message" required> <br>
+<button>Envoyer</button>
+
+<footer class="footer">
+        <p>© 2024 MonSiteWeb | <a href="mailto:contact@monsite.com">Contactez-nous</a></p>
+        <p>Numéro de téléphone: 06 12 45 74 51git  </p>
+    </footer>
+
+    <a href="https://www.bing.com/maps?osid=5075465a-3e75-4916-8a52-b91ad6bc9727&cp=48.788715~2.361106&lvl=17&pi=0&imgid=c5801d5e-0805-4f4e-bac8-2d57de9b4cd9&v=2&sV=2&form=S00027" target="_blank">Localisation : Google Maps</a>
+
+</from>
